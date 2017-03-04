acb5c00731595035e9849a2577dd676ad38750d5
andreuinyu <andreu4ps@gmail.com>
2017-03-04 03:15:06 +0100

    primera

--- main.js
+++ main.js
@@ -1,50 +1,11 @@
 /**
  * Created by andre on 03/03/2017.
  */
-<<<<<<< HEAD
 
-Function string2date(s) { //s is a string that contains the date in format dd/mm/yyyy hh:mm:ss
-	var d =new Date();
-	//4/2/2017 1:59:30
-	var date_hour = s.split(" ");
-	var date = date_hour[0].split("/");
-	var hour = date_hour[1].split(":");
-	d.setDay(parseInt(date[0]);
-	d.setMonth(parseInt(date[1]);
-	d.setYear(parseInt(date[2]);
-	d.setHour(parseInt(hour[0]);
-	d.setMinute(parseInt(hour[1]);
-	d.setSeconds(parseInt(hour[2]);
-	}
- 
-//  
- 
-class Point{
-	constructor(lat, lng, time){
-		
-		this.lat = lat;
-		this.lng = lng;
-		this.time= data(time); 
-		}
-		
-	show(){
-		var marker = new google.maps.Marker({
-            position: {lat: this.lat, lng: this.lng},
-            map: map,
-            zIndex: pupa[3],
-            draggable: false,
-		}	
-	}
- 
-var Arrayarnos = [];
-
- 
-=======
->>>>>>> origin/master
 var pupas = [
-    new Point(41.492009, 2.362017, ""),
-    new Point(41.555833, 2.4025,""),
-    new Point(41.533333, 2.45,"")
+    new Point(41.492009, 2.362017, "5/2/2017 1:59:30"),
+    new Point(41.555833, 2.4025,"4/2/2017 1:59:30"),
+    new Point(41.533333, 2.45,"6/2/2017 1:59:30")
 ];
 
 function initMap() {
@@ -58,5 +19,6 @@ function initMap() {
 function setMarkers(map) {
     for (var i = 0; i < pupas.length; i++) {
         pupas[i].show(map);
+        console.log(pupas[i].time);
     }
 }
--- point.js
+++ point.js
@@ -1,7 +1,7 @@
 var Point = function(lat, lng, time){
     this.lat = lat;
     this.lng = lng;
-    this.time = time;
+    this.time = string2date(time);
 
     this.show = function(map){
         var marker = new google.maps.Marker({
@@ -13,4 +13,19 @@ var Point = function(lat, lng, time){
             icon: 'point_animation.gif'
         });
     };
+}
+
+function string2date(s) {
+    //s is a string that contains the date in format 4/2/2017 1:59:30
+    var d = new Date();
+    var date_hour = s.split(" ");
+    var date = date_hour[0].split("/");
+    var hour = date_hour[1].split(":");
+    d.setDate(parseInt(date[0]));
+    d.setMonth(parseInt(date[1]));
+    d.setFullYear(parseInt(date[2]));
+    d.setHours(parseInt(hour[0]));
+    d.setMinutes(parseInt(hour[1]));
+    d.setSeconds(parseInt(hour[2]));
+    return d;
 }
