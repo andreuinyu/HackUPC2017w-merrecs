b817556ff4438608dc8018afc5791955293a82d2
Andreu Punsola Soler <andreu4ps@gmail.com>
2017-03-05 12:42:54 +0100

    Update server.php

--- server.php
+++ server.php
@@ -1,20 +1,17 @@
 <?php
-$socket = socket_create_listen(5354);
+$socket = socket_create_listen("tcp://0.0.0.0:5354");
 if (!$socket) {
   echo "\n";
 } else {
-  while ($conn = socket_accept($socket)) {
+  while ($conn = socket_accept_stream($socket)) {
     $data_in = fread($conn,50);
     fwrite($conn, 'Your emergency request has been received at ' . date('n/j/Y g:i a') . "\n");
     fclose($conn);
 
     $file = "./database.txt";
-
-    // string to put username and passwords
     $entry = '';// file_get_contents($file);
     // check for empty indexes
     if (!empty($data_in)) {
-
         $entry .= $data_in;
         $entry .= "\r\n";
         // using file_put_contents() instead of fwrite()
