26d0183ec991511b0c028b6b402e0f625d85741b
Andreu Punsola Soler <andreu4ps@gmail.com>
2017-03-05 12:34:18 +0100

    Update server.php

--- server.php
+++ server.php
@@ -1,9 +1,9 @@
 <?php
-$socket = stream_socket_server("tcp://0.0.0.0:5354", $errno, $errstr);
+$socket = socket_create_listen("tcp://0.0.0.0:5354", $errno, $errstr);
 if (!$socket) {
   echo "$errstr ($errno)<br />\n";
 } else {
-  while ($conn = stream_socket_accept($socket)) {
+  while ($conn = socket_accept(($socket)) {
     $data_in = fread($conn,50);
     fwrite($conn, 'Your emergency request has been received at ' . date('n/j/Y g:i a') . "\n");
     fclose($conn);
