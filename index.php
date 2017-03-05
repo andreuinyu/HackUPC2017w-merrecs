ce358f2706fadda8850acd0b3e748ab1ef933fa1
andreuinyu <andreu4ps@gmail.com>
2017-03-05 03:47:05 +0100

    .

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
     </p>
 </div>
 <div id="map"></div>
-
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBAVldevQ9URKGx2yxZ4or9YdBtdnmkjh8&callback=initMap&language=en">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 <script language="javascript" type="text/javascript" src="point.js"></script>
 <script language="javascript" type="text/javascript" src="main.js"></script>
@@ -63,7 +63,6 @@ var poll = function () {
   };
   poll();
 </script>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBAVldevQ9URKGx2yxZ4or9YdBtdnmkjh8&callback=initMap&language=en">
 </script>
 </body>
 </html>
